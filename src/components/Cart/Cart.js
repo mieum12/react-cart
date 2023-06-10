52156887110d25eeba6705426cf251639fe031b6
mieum12 <qpdlqltb1215@gmail.com>
2023-06-10 19:43:34 +0900

    6/10 order 완료 후 완료 메세지 띄우기

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,14 @@
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
 import Checkout from "./Checkout";
 
 const Cart = (props) => {
   const [isCheckout, setIsCheckout] = useState(false);
-
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submitIsDone, setSubmitIsDone] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -28,14 +29,20 @@ const Cart = (props) => {
   };
 
   //checkout 컴포넌트에서 전달받은 사용자 정보(userData)를 가져온 뒤 백엔드로 전송하기!!
-  const submitOrderHandler = (userData) => {
-    fetch("https://react-http-510c3-default-rtdb.firebaseio.com/order.json", {
-      method: "POST",
-      body: JSON.stringify({
-        user: userData,
-        orderedItems: cartCtx.items,
-      }),
-    });
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    await fetch(
+      "https://react-http-510c3-default-rtdb.firebaseio.com/order.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items,
+        }),
+      }
+    );
+    setIsSubmitting(false);
+    setSubmitIsDone(true);
   };
 
   const cartItems = (
@@ -66,8 +73,9 @@ const Cart = (props) => {
     </div>
   );
 
-  return (
-    <Modal onClose={props.onClose}>
+  //1. 아직 쇼핑중~ 제출 전인 상태
+  const cartModalContent = (
+    <React.Fragment>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -77,6 +85,30 @@ const Cart = (props) => {
         <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
       )}
       {!isCheckout && modalActions}
+    </React.Fragment>
+  );
+
+  //2. 제출중인 경우
+  const isSubmittingModalContent = <p>주문을 접수하는 중입니다!</p>;
+
+  //3. 주문서가 제출 완료된 경우
+  const submitIsDoneModalContent = (
+    //React.Fragment로 감싸는 이유는 형제 JSX를 만들기 위해 하나의 태그로 감싸주는 것
+    <React.Fragment>
+      <p>주문이 성공적으로 접수되었습니다!</p>
+      <div className={classes.actions}>
+        <button className={classes.button} onClick={props.onClose}>
+          Close
+        </button>
+      </div>
+    </React.Fragment>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !submitIsDone && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && submitIsDone && submitIsDoneModalContent}
     </Modal>
   );
 };
